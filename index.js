bba1287be3131825e6781fc2845a71896c549c07
Tran Khanh Duong <duonggggg177@gmail.com>
2025-06-12 02:53:25 +0700

    danh sach san pham ben admin, loc theo trang thai san pham

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const database = require("./config/database")
 require("dotenv").config();
+
+const database = require("./config/database")
 const route = require("./routes/client/client.route");
+const routeAdmin = require("./routes/admin/admin.route");
+const systemConfig = require("./config/system")
 const app = express();
 const port = process.env.PORT;
-
 database.connect();
-
 app.set("views", "./views");
 app.set("view engine", "pug");
-
 app.use(express.static("public"));
 
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
