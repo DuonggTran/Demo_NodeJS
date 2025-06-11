b2858e404cad79b7b3347edd6607dc50acf666f2
Tran Khanh Duong <duonggggg177@gmail.com>
2025-06-11 21:46:48 +0700

    connect database, lam trang danh sach san pham ben client

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const database = require("./config/database")
 require("dotenv").config();
 const route = require("./routes/client/client.route");
 const app = express();
 const port = process.env.PORT;
 
+database.connect();
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.use("/",route)
+app.use(express.static("public"));
+
+route(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
